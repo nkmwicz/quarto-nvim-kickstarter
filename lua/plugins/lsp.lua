450057e8f163e9b50f97dad21dcf72d6cc551b62
Nathan Michalewicz <nathan@mwicz.com>
2025-01-01 12:35:07 -0500

    update lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -46,34 +46,6 @@ return {
             end)(),
           },
         },
-        on_attach = function(client, bufnr)
-          -- Enable completion if you are using nvim-cmp
-          local function buf_set_option(opt, value)
-            vim.api.nvim_buf_set_option(bufnr, opt, value)
-          end
-          buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-          -- Mappings.
-          local opts = { noremap=true, silent=true }
-          vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-          vim.keymap.set('n', '<leader>k', vim.lsp.buf.hover, opts)
-          vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-          vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-          vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-          vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-          vim.keymap.set('n', '<space>wl', function()
-            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-          end, opts)
-          vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-          vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-          vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, opts)
-          vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
-          vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-          vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-          vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
-          vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, opts)
-        end,
       }
     end,
   },
@@ -408,8 +380,6 @@ return {
           return util.root_pattern('.git', 'setup.py', 'setup.cfg', 'pyproject.toml', 'requirements.txt')(fname) or util.path.dirname(fname)
         end,
       }
-
-      -- vim.api.nvim_set_keymap('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', { noremap = true, silent = true })
     end,
   },
 }
