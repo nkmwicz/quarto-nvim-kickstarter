63a88b83f4c47bef54efdfe003100b554dcbdb53
Nathan Michalewicz <nathan@mwicz.com>
2025-01-22 10:19:02 -0500

    Fix Foldings

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -82,16 +82,38 @@ return {
             },
           },
         },
-        fold = {
-          enable = true,
-          indent_levels = true,
+        -- fold = {
+        --   enable = true,
+        --   indent_levels = true,
+        -- },
+      }
+      vim.o.foldmethod = 'expr'
+      vim.o.foldexpr = 'nvim_treesitter#foldexpr()' -- treesitter folding
+      -- vim.opt.foldtext = "v:folddashes.repeat(v:foldlevel)..' ' .. v:foldstart .. ' lines: ' .. (v:foldend - v:foldstart + 1)"
+      vim.api.nvim_set_keymap('n', '<Tab>', 'za', { noremap = true, silent = true })
+      local vim = vim
+      local api = vim.api
+      local M = {}
+      function M.nvim_create_augroups(definitions)
+        for group_name, definition in pairs(definitions) do
+          api.nvim_command('augroup ' .. group_name)
+          api.nvim_command 'autocmd!'
+          for _, def in ipairs(definition) do
+            local command = table.concat(vim.tbl_flatten { 'autocmd', def }, ' ')
+            api.nvim_command(command)
+          end
+          api.nvim_command 'augroup END'
+        end
+      end
+
+      local autoCommands = {
+        -- other autocommands
+        open_folds = {
+          { 'BufReadPost,FileReadPost', '*', 'normal zR' },
         },
       }
-      vim.o.foldmethod = 'indent'
-      vim.o.foldexpr = '' -- treesitter folding
-      vim.opt.foldtext = "v:folddashes.repeat(v:foldlevel)..' ' .. v:foldstart .. ' lines: ' .. (v:foldend - v:foldstart + 1)"
 
-
+      M.nvim_create_augroups(autoCommands)
     end,
   },
 }
