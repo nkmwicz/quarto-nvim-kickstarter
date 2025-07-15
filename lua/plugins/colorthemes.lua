90e72a52686e4da6255d8ec6da9dbf056d57dd8a
Nathan Michalewicz <nathan@mwicz.com>
2025-07-15 16:51:22 -0400

    Comment out color themes

--- lua/plugins/colorthemes.lua
+++ lua/plugins/colorthemes.lua
@@ -1,35 +1,35 @@
 return {
-  { 'shaunsingh/nord.nvim', enabled = true, lazy = false, priority = 1000 },
-  { 'folke/tokyonight.nvim', enabled = true, lazy = false, priority = 1000 },
-  { 'EdenEast/nightfox.nvim', enabled = true, lazy = false, priority = 1000 },
-  { 'Mofiqul/vscode.nvim', enabled = true, lazy = false, priority = 1000 },
-  {
-    'catppuccin/nvim',
-    name = 'catppuccin',
-    enabled = true,
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      -- set colorscheme and overwrite highlights
-      vim.cmd.colorscheme 'catppuccin-mocha'
-      local colors = require 'catppuccin.palettes.mocha'
-      vim.api.nvim_set_hl(0, 'Tabline', { fg = colors.green, bg = colors.mantle })
-      vim.api.nvim_set_hl(0, 'TermCursor', { fg = '#A6E3A1', bg = '#A6E3A1' })
-    end,
-  },
+  -- { 'shaunsingh/nord.nvim', enabled = true, lazy = false, priority = 1000 },
+  -- { 'folke/tokyonight.nvim', enabled = true, lazy = false, priority = 1000 },
+  -- { 'EdenEast/nightfox.nvim', enabled = true, lazy = false, priority = 1000 },
+  -- { 'Mofiqul/vscode.nvim', enabled = true, lazy = false, priority = 1000 },
+  -- {
+  --   'catppuccin/nvim',
+  --   name = 'catppuccin',
+  --   enabled = true,
+  --   lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  --   priority = 1000, -- make sure to load this before all the other start plugins
+  --   config = function()
+  --     -- set colorscheme and overwrite highlights
+  --     vim.cmd.colorscheme 'catppuccin-mocha'
+  --     local colors = require 'catppuccin.palettes.mocha'
+  --     vim.api.nvim_set_hl(0, 'Tabline', { fg = colors.green, bg = colors.mantle })
+  --     vim.api.nvim_set_hl(0, 'TermCursor', { fg = '#A6E3A1', bg = '#A6E3A1' })
+  --   end,
+  -- },
 
-  {
-    'oxfist/night-owl.nvim',
-    enabled = false,
-    lazy = false,
-    priority = 1000,
-    config = function()
-      -- load the colorscheme here
-      require('night-owl').setup()
-      vim.cmd.colorscheme 'night-owl'
-      vim.api.nvim_set_hl(0, 'TermCursor', { fg = '#A6E3A1', bg = '#A6E3A1' })
-    end,
-  },
+  -- {
+  --   'oxfist/night-owl.nvim',
+  --   enabled = false,
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     -- load the colorscheme here
+  --     require('night-owl').setup()
+  --     vim.cmd.colorscheme 'night-owl'
+  --     vim.api.nvim_set_hl(0, 'TermCursor', { fg = '#A6E3A1', bg = '#A6E3A1' })
+  --   end,
+  -- },
 
   {
     'rebelot/kanagawa.nvim',
