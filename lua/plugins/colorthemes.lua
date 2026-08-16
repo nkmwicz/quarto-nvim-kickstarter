a1ccc58bf4a37c33e5d8d56ebdb409a42c826336
Nathan Michalewicz <nathan@mwicz.com>
2026-08-16 11:48:10 -0400

    Uncomment catppuccin spec (disabled) for easy future testing
    Kept enabled = false since kanagawa remains the active colorscheme.

--- lua/plugins/colorthemes.lua
+++ lua/plugins/colorthemes.lua
@@ -3,20 +3,29 @@ return {
   -- { 'folke/tokyonight.nvim', enabled = true, lazy = false, priority = 1000 },
   -- { 'EdenEast/nightfox.nvim', enabled = true, lazy = false, priority = 1000 },
   -- { 'Mofiqul/vscode.nvim', enabled = true, lazy = false, priority = 1000 },
-  -- {
-  --   'catppuccin/nvim',
-  --   name = 'catppuccin',
-  --   enabled = true,
-  --   lazy = false, -- make sure we load this during startup if it is your main colorscheme
-  --   priority = 1000, -- make sure to load this before all the other start plugins
-  --   config = function()
-  --     -- set colorscheme and overwrite highlights
-  --     vim.cmd.colorscheme 'catppuccin-mocha'
-  --     local colors = require 'catppuccin.palettes.mocha'
-  --     vim.api.nvim_set_hl(0, 'Tabline', { fg = colors.green, bg = colors.mantle })
-  --     vim.api.nvim_set_hl(0, 'TermCursor', { fg = '#A6E3A1', bg = '#A6E3A1' })
-  --   end,
-  -- },
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    enabled = false,
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require('catppuccin').setup({
+        flavour = 'mocha',
+        integrations = {
+          treesitter = true,
+          native_lsp = { enabled = true },
+          markdown = true,
+          telescope = { enabled = true },
+        },
+      })
+      -- set colorscheme and overwrite highlights
+      vim.cmd.colorscheme 'catppuccin-mocha'
+      local colors = require 'catppuccin.palettes.mocha'
+      vim.api.nvim_set_hl(0, 'Tabline', { fg = colors.green, bg = colors.mantle })
+      vim.api.nvim_set_hl(0, 'TermCursor', { fg = '#A6E3A1', bg = '#A6E3A1' })
+    end,
+  },
 
   -- {
   --   'oxfist/night-owl.nvim',
